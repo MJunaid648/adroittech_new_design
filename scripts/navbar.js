4bd34e978dbfd2118aaaa33a0db9aa778fc2a2d7
Muhammad Junaid <648mjunaid@gmail.com>
2023-12-06 20:50:31 +0500

    completed

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,14 +1,17 @@
 function hamClick() {
   const hamberger = document.querySelector(".hamberger");
   const mobNav = document.querySelector(".mobnav");
+  const hamburgerIcon = document.getElementById("hamburgerIcon");
 
   if (mobNav.classList.contains("drawerOpen")) {
     hamberger.style.color = "#FFF";
     mobNav.classList.add("drawerClosed");
     mobNav.classList.remove("drawerOpen");
+    hamburgerIcon.src = "./assets/hamberger.svg";
   } else if (mobNav.classList.contains("drawerClosed")) {
     hamberger.style.color = "#FFF";
     mobNav.classList.add("drawerOpen");
     mobNav.classList.remove("drawerClosed");
+    hamburgerIcon.src = "./assets/menu cross.svg ";
   }
 }
